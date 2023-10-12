ceaee2992dd7ff3e9a8c4fc1a395e39aa4ee23a9
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-12 19:02:40 +0700

    chore: add retry config

--- resilience4j/src/test/java/org/example/RetryTest.java
+++ resilience4j/src/test/java/org/example/RetryTest.java
@@ -1,8 +1,12 @@
 package org.example;
 
 import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.function.Supplier;
 
 @Slf4j
 public class RetryTest {
@@ -12,6 +16,11 @@ public class RetryTest {
         throw new IllegalArgumentException("Error in callMe()");
     }
 
+    String sayHello(){
+        log.info("try say hello");
+        throw new IllegalArgumentException("Error in sayHello()");
+    }
+
     @Test
     void testRetry() {
         Retry retry = Retry.ofDefaults("ichwan");
@@ -19,5 +28,20 @@ public class RetryTest {
         //jika method callMe() mengembalikan nilai, gunakan decorateSupplier()
         Runnable runnable = Retry.decorateRunnable(retry, () -> callMe());
         runnable.run();
+    }
+
+    @Test
+    void testRetryConfig(){
+        RetryConfig config = RetryConfig.custom()
+                .maxAttempts(5)
+                .waitDuration(Duration.ofSeconds(1))
+                .retryExceptions(IllegalArgumentException.class)
+                .ignoreExceptions(RuntimeException.class)
+                .build();
+
+        Retry retry = Retry.of("ichwan", config);
+
+        Supplier<String> supplier = Retry.decorateSupplier(retry, () -> sayHello());
+        supplier.get();
     }
 }
