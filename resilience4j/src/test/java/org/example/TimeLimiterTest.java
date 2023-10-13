7cd5efab79c819a4db78684809b047dd753d5b36
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-13 17:24:24 +0700

    chore: add time limiter module

---
+++ resilience4j/src/test/java/org/example/TimeLimiterTest.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import io.github.resilience4j.timelimiter.TimeLimiter;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterRegistry;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+@Slf4j
+public class TimeLimiterTest {
+
+    @SneakyThrows
+    public String slow(){
+        log.info("Slow");
+        Thread.sleep(5000L);
+        return "Eko";
+    }
+
+    @Test
+    void timeLimiter() throws Exception {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<String> future = executorService.submit(() -> slow());
+
+        TimeLimiter timeLimiter = TimeLimiter.ofDefaults("ichwan");
+        Callable<String> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> future);
+
+        callable.call();
+    }
+
+    @Test
+    void timeLimiterConfig() throws Exception {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<String> future = executorService.submit(() -> slow());
+
+        TimeLimiterConfig config = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(10))
+                .cancelRunningFuture(true)
+                .build();
+
+        TimeLimiter timeLimiter = TimeLimiter.of("ichwan", config);
+        Callable<String> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> future);
+
+        callable.call();
+    }
+
+    @Test
+    void timeLimiterRegistry() throws Exception {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<String> future = executorService.submit(() -> slow());
+
+        TimeLimiterConfig config = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(10))
+                .cancelRunningFuture(true)
+                .build();
+
+        TimeLimiterRegistry registry = TimeLimiterRegistry.ofDefaults();
+        registry.addConfiguration("config",config);
+
+        TimeLimiter timeLimiter = registry.timeLimiter("ichwan","config");
+        Callable<String> callable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> future);
+
+        callable.call();
+    }
+}
