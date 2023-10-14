db0abc690a48cb51133ac8b512d1c5bfccd7071a
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-15 03:54:52 +0700

    chore: add output stream and writer

---
+++ io/src/test/java/org/example/OutputStreamTest.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class OutputStreamTest {
+
+    @Test
+    void writeOutputStream() {
+
+        Path path = Path.of("output.txt");
+        try(OutputStream stream = Files.newOutputStream(path)) {
+            for (int i = 0; i < 50; i++) {
+                stream.write("Write data\n".getBytes());
+                stream.flush();
+            }
+        } catch (IOException e){
+            Assertions.fail(e);
+        }
+    }
+    
+    @Test
+    void testWriter(){
+        Path path = Path.of("writer.txt");
+        try(Writer writer = Files.newBufferedWriter(path)) {
+            for (int i = 0; i < 50; i++) {
+                writer.write("Write data\n");
+                writer.flush();
+            }
+        } catch (IOException e){
+            Assertions.fail(e);
+        }
+    }
+}
