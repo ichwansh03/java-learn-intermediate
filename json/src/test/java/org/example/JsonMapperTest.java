dc96844e1b097dfa454eb09698a04813361806a3
ichwansh03 <ichwansholihin03@gmail.com>
2023-10-16 05:45:16 +0700

    chore: add object to json manipulation

--- json/src/test/java/org/example/JsonMapperTest.java
+++ json/src/test/java/org/example/JsonMapperTest.java
@@ -22,12 +22,12 @@ class JsonMapperTest {
     @Test
     void createJson() throws JsonProcessingException {
         Map<String, Object> data = Map.of(
-                "name","Ichwan Sholihin",
-                "address",Map.of(
+                "name", "Ichwan Sholihin",
+                "address", Map.of(
                         "street", "Rajawali",
                         "city", "Bandar Lampung"
                 ),
-                "age",20,
+                "age", 20,
                 "married", true
         );
 
@@ -56,11 +56,41 @@ class JsonMapperTest {
      */
     @Test
     void createJsonArray() throws JsonProcessingException {
-        List<String> state = List.of("Indonesia","Palestine","Yemen");
+        List<String> state = List.of("Indonesia", "Palestine", "Yemen");
 
         String value = objectMapper.writeValueAsString(state);
 
         Assertions.assertNotNull(state);
         System.out.println(value);
+    }
+
+    @Test
+    void createJsonFromObject() throws JsonProcessingException {
+        Person person = new Person();
+
+        person.setId("123A");
+        person.setName("Ichwan Sholihin");
+        person.setHobbies(List.of("Coding", "Reading", "Travelling"));
+
+        Address address = new Address();
+        address.setStreet("Rajawali");
+        address.setCity("Bandar Lampung");
+        address.setZipcode("35362");
+        person.setAddress(address);
+
+        String value = objectMapper.writeValueAsString(person);
+        Assertions.assertNotNull(person);
+        System.out.println(value);
+    }
+
+    @Test
+    void convertJsonToObject() throws JsonProcessingException {
+        String json = """
+                {"id":"123A","name":"Ichwan Sholihin","hobbies":["Coding","Reading","Travelling"],"address":{"street":"Rajawali","city":"Bandar Lampung","zipcode":"35362"}}
+                """;
+
+        Person person = objectMapper.readValue(json, Person.class);
+
+        Assertions.assertNotNull(person);
     }
 }
